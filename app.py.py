1b58b34baecb6035239d3e12a2d1c441b9eb2f76
hotaq <131946401+hotaq@users.noreply.github.com>
2024-06-10 23:28:07 +0700

    Add files via upload

---
+++ app.py.py
@@ -0,0 +1,113 @@
+from pathlib import Path
+import streamlit as st
+from ultralytics import YOLO
+from PIL import Image
+def load_model(model_path):
+    """
+    Loads a YOLO object detection model from the specified model_path.
+
+    Parameters:
+        model_path (str): The path to the YOLO model file.
+
+    Returns:
+        A YOLO object detection model.
+    """
+    model = YOLO(model_path)
+    return model
+
+def infer_uploaded_image(conf, model):
+    """
+    Execute inference for uploaded image
+    :param conf: Confidence of YOLOv8 model
+    :param model: An instance of the `YOLOv8` class containing the YOLOv8 model.
+    :return: None
+    """
+    source_img = st.sidebar.file_uploader(
+        label="Choose an image...",
+        type=("jpg", "jpeg", "png", 'bmp', 'webp')
+    )
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if source_img:
+            uploaded_image = Image.open(source_img)
+            # adding the uploaded image to the page with caption
+            st.image(
+                image=source_img,
+                caption="Uploaded Image",
+                use_column_width=True
+            )
+    if source_img:
+        if st.button("Execution"):
+            with st.spinner("Running..."):
+                res = model.predict(uploaded_image,
+                                    conf=conf)
+                boxes = res[0].boxes
+                res_plotted = res[0].plot()[:, :, ::-1]
+
+                with col2:
+                    st.image(res_plotted,
+                             caption="Detected Image",
+                             use_column_width=True)
+                    try:
+                        with st.expander("Detection Results"):
+                            for box in boxes:
+                                st.write(box.xywh)
+                    except Exception as ex:
+                        st.write("No image is uploaded yet!")
+                        st.write(ex)
+
+st.set_page_config(
+    page_title="Interactive Interface for YOLOv8",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="expanded"
+    )
+
+# main page heading
+st.title("Interactive Interface for YOLOv8")
+
+# sidebar
+st.sidebar.header("DL Model Config")
+
+# model options
+task_type = st.sidebar.selectbox(
+    "Select Task",
+    ["Detection"]
+)
+
+model_type = None
+if task_type == "Detection":
+    model_type = st.sidebar.selectbox(
+        "Select Model",
+        ['yolov8']
+    )
+else:
+    st.error("Currently only 'Detection' function is implemented")
+
+confidence = float(st.sidebar.slider(
+    "Select Model Confidence", 30, 100, 50)) / 100
+
+
+if model_type:
+    model_path = Path('C:/Users/ADMINS/Downloads/AI_builder-main/AI_builder-main/models/yolov8.pt')
+else:
+    st.error("Please Select Model in Sidebar")
+
+# load pretrained DL model
+try:
+    model = load_model(model_path)
+except Exception as e:
+    st.error(f"Unable to load model. Please check the specified path: C:/Users/ADMINS/Downloads/AI_builder-main/AI_builder-main/models/yolov8.pt")
+
+# image/video options
+st.sidebar.header("Image/Video Config")
+source_selectbox = st.sidebar.selectbox(
+    "Select Source",
+    ['Image']
+)
+
+source_img = None
+if source_selectbox == 'Image': # Image
+    infer_uploaded_image(confidence, model)
